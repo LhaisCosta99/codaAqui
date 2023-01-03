6795535476554dd6841755dae113f9b01e8c62eb
LhaisCosta99 <lhaisferreirada@gmail.com>
2023-01-03 14:02:36 -0300

    inserção e aplicação de lógica para codificação

--- codifica.js
+++ codifica.js
@@ -1,4 +1,26 @@
-const mesagem = document.getElementById("mensagem")
+const mensagem = document.getElementById("mensagem")
+const criptografar = document.getElementById("criptografar")
+const decriptografar = document.getElementById("decriptografar")
+const resultado = document.getElementById("resultado")
+
+criptografar.addEventListener("click", (event)=> {
+    event.preventDefault()
+    if(mensagem.value){
+        const resposta = codificador(mensagem.value)
+    
+        resultado.innerHTML = `
+            <h2>Resultado da sua mensagem:</h2>
+            <p>${resposta}</p>
+        `
+    
+        mensagem.value = ""
+    } else {
+        resultado.innerHTML = `
+        <h2>Nenhuma mensagem encontrada</h2>
+        <p>Digite um texto que você deseja criptografar ou descriptografar.</p>
+        `
+    }
+})
 
 const dicionario = {
     a: "ai",
@@ -8,7 +30,7 @@ const dicionario = {
     u: "ufat"
 }
 
-function conversor (string){
+function codificador (string){
     const entrada = string.split("")
 
     const saida = entrada.map(el => {
